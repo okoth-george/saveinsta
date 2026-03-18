0a3d3158724f383bb98442daa45f9c380d637627
okoth-george <okothgeorge911@gmail.com>
2026-03-18 13:17:41 +0300

    add production env config and update CORS for Vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173',                        // local dev
-    'https://saveinsta.vercel.app',                 // Vercel production
+    'https://saveinsta-jade.vercel.app/',                 // Vercel production
     process.env.FRONTEND_URL,                       // from env var
   ].filter(Boolean),
   methods: ['GET', 'POST'],
